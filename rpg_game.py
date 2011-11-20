1416fd61c067fdd4e770b413cf1d39ca710badd0
Daniel Gottlieb <danny.gottlieb@gmail.com>
2011-11-20 12:44:03 -0500

    we have walking and motion

--- rpg_game.py
+++ rpg_game.py
@@ -10,24 +10,24 @@ import pygame
 pygame.init()
 pygame.mouse.set_visible(0)
 
-black = (0, 0, 0)
+TPS = 2 #Twitches per second, characters transitioning between different states
+MPS = 3 #Movement per second, how many tiles the hero can walk per second
 
-FPS = 30
-MPS = 2
-
-screen = tiled_screen.Screen(rows=12, columns=12, tile_width=64, tile_height=64)
+screen = tiled_screen.Screen(rows=9, columns=13, tile_width=64, tile_height=64)
 
 chars_sheet = spritesheet.SpriteSheet("images/chars.png", background_color=(135, 191, 255, 255))
 tiles_sheet = spritesheet.SpriteSheet("images/tiles.png", background_color=(0, 128, 128, 255))
 
 hero = [chars_sheet.get_item(1, x, 64, 64, transparent=True) for x in range(8)]
-hero_rect = hero[0].get_rect()
+screen.set_hero_down(hero[0:2])
+screen.set_hero_right(hero[2:4])
+screen.set_hero_left(hero[4:6])
+screen.set_hero_up(hero[6:8])
+screen.set_hero_tps(TPS)
+screen.set_walking_speed(MPS)
 
 brick_tile = tiles_sheet.get_item(0, 3, 64, 64)
-brick_rect = brick_tile.get_rect()
-
-hero_rect[0] = screen.width / 2
-hero_rect[1] = screen.height / 2
+screen.tile(brick_tile)
 
 total_frames = 0
 
@@ -39,24 +39,41 @@ while game_on:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             game_on = False
+            continue
 
         if event.type == pygame.KEYUP:
             if event.dict['key'] == 27:
+                #Escape
                 game_on = False
+                continue
 
-    screen.screen.fill(black)
-    screen.screen.blit(brick_tile, brick_rect)
-    screen.screen.blit(hero[total_frames / (FPS / MPS) % len(hero)], hero_rect)
+        if event.type == pygame.KEYDOWN:
+            if event.dict['key'] == 273:
+                #Up key
+                #screen.walking_up()
+                screen.walking_up()
+            if event.dict['key'] == 276:
+                #Left
+                screen.walking_left()
+            if event.dict['key'] == 274:
+                #Down
+                screen.walking_down()
+            if event.dict['key'] == 275:
+                #Right
+                screen.walking_right()
 
-    pygame.display.flip()
-    total_frames += 1
+        if event.type == pygame.KEYUP:
+            if event.dict['key'] in (273, 274, 275, 276):
+                #Any array key
+                screen.stop_walking()
 
+    screen.draw()
     clock.tick(30)
 
 end = time.time()
 print 'Quitting...'
 print 'Total Time = %.2f' % (end-start)
-print 'Total Frames = %d' % (total_frames)
-print 'Average FPS = %.2f' % (total_frames / (end-start))
+print 'Total Frames = %d' % (screen.total_frames)
+print 'Average FPS = %.2f' % (screen.total_frames / (end-start))
 
 pygame.quit()
--- tiled_screen.py
+++ tiled_screen.py
@@ -1,17 +1,122 @@
 import pygame
 
+black = (0, 0, 0)
+FPS = 30
+
 class Screen(object):
     def __init__(self, rows, columns, tile_width, tile_height):
-        self.rows = rows
-        self.columns = columns
+        self.rows = rows + 2
+        self.columns = columns + 2
         self.tile_width = tile_width
         self.tile_height = tile_height
+        self.grid = [[None for y in range(self.columns)] for x in range(self.rows)]
 
         self.width = tile_width * columns
         self.height = tile_height * rows
 
         self.screen = pygame.display.set_mode(self.size)
+        self.total_frames = 0
+
+        self.hero_rect = (columns/2 * tile_width, rows/2 * tile_height,
+                          tile_width, tile_height)
+
+        self.motioning = False
+        self.row_offset = 0
+        self.col_offset = 0
 
     @property
     def size(self):
         return (self.width, self.height)
+        
+    def tile(self, tile):
+        for row in self.grid:
+            for y in range(len(row)):
+                row[y] = tile
+
+    def set_hero_up(self, hero_up_animations):
+        self.hero_up = hero_up_animations
+        self._hero_orientation = self.hero_up
+
+    def set_hero_left(self, hero_left_animations):
+        self.hero_left = hero_left_animations
+
+    def set_hero_down(self, hero_down_animations):
+        self.hero_down = hero_down_animations
+
+    def set_hero_right(self, hero_right_animations):
+        self.hero_right = hero_right_animations
+
+    def moving(self, row_change, col_change):
+        self.motioning = True
+        self.start_frame = self.total_frames
+        self.num_frames = FPS / self.mps
+
+        self.moving_rows = row_change
+        self.moving_cols = col_change
+
+    def walking_up(self):
+        self.moving(1, 0)
+        self._hero_orientation = self.hero_up
+
+    def walking_down(self):
+        self.moving(-1, 0)
+        self._hero_orientation = self.hero_down
+
+    def walking_left(self):
+        self.moving(0, 1)
+        self._hero_orientation = self.hero_left
+
+    def walking_right(self):
+        self.moving(0, -1)
+        self._hero_orientation = self.hero_right
+
+    def set_hero_tps(self, tps):
+        self.hero_tps = tps
+
+    def set_walking_speed(self, mps):
+        self.mps = mps
+
+    def stop_walking(self):
+        self.moving_rows = 0
+        self.moving_cols = 0
+
+    def blit_map(self):
+        self.motion()
+        for row_idx in range(len(self.grid)):
+            for col_idx in range(len(self.grid[row_idx])):
+                tile = self.grid[row_idx][col_idx]
+                start_x = self.tile_width * (col_idx - 1) + self.col_offset
+                start_y = self.tile_height * (row_idx - 1) + self.row_offset
+                self.screen.blit(tile, (start_x, start_y, self.tile_width, self.tile_height))
+
+    def blit_hero(self):
+        frames_per_twitch = (FPS / self.hero_tps)
+        hero_frame_idx = (self.total_frames / frames_per_twitch) % len(self._hero_orientation)
+        self.screen.blit(self._hero_orientation[hero_frame_idx], self.hero_rect)
+
+    def motion(self):
+        if self.motioning == False:
+            return
+
+        perc_change = 1.0 * (self.total_frames - self.start_frame) / self.num_frames
+        if perc_change < 1.0:
+            self.row_offset = int(self.moving_rows * self.tile_height * perc_change)
+            self.col_offset = int(self.moving_cols * self.tile_width * perc_change)
+        else:
+            if self.moving_rows == 0 and self.moving_cols == 0:
+                #button was let go, stop motioning
+                self.row_offset = 0
+                self.col_offset = 0
+                self.motioning = False
+            else:
+                #button is still held down, let's
+                self.moving(self.moving_rows, self.moving_cols)
+
+    def draw(self):
+        self.screen.fill(black)
+        self.blit_map()
+        self.blit_hero()
+
+        pygame.display.flip()
+
+        self.total_frames += 1
